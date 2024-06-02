0de2356a62579a25a89217a40b03367677b4eb99
seungaeahn <seungae.ahn418@gmail.com>
2024-06-02 22:13:35 +0900

    로그인화면 구현

---
+++ front/src/constants/colors.ts
@@ -0,0 +1,13 @@
+const colors = {
+    GREEN_500: '#0d6f53',
+    GREEN_700: '#006643',
+    WHITE: '#ffff',
+    GRAY_200: '#e7e7e7',
+    GRAY_500: '#8e8e8e',
+    GRAY_700: '#575757',
+    BLACK: '#000',
+    RED_300: '#ffb4b4',
+    RED_500: '#ff5f5f',
+  };
+  
+  export {colors};
---
+++ front/src/navigations/root/RootNavigator.tsx
@@ -0,0 +1,11 @@
+
+import MainDrawerNavigator from '../drawer/MainDrawerNavigator';
+import AuthStackNavigator from '../stack/AuthStackNavigator';
+
+function RootNavigator() {
+  const isLoggedIn = false;
+
+  return <>{isLoggedIn ? <MainDrawerNavigator /> : <AuthStackNavigator />}</>;
+}
+
+export default RootNavigator;
