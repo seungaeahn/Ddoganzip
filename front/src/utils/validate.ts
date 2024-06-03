e40f71f6a62aeb5794c394a2afa71aa0da955e9b
seungaeahn <seungae.ahn418@gmail.com>
2024-06-03 13:27:09 +0900

    Membership registration screen

--- front/src/utils/validate.ts
+++ front/src/utils/validate.ts
@@ -1,27 +1,37 @@
-type UserInformation = {
+type UserInfomation = {
   email: string;
   password: string;
 };
 
-function validateLogin(values: UserInformation) {
+function validateUser(values: UserInfomation) {
   const errors = {
     email: '',
     password: '',
   };
 
-  if (
-    !/^[0-9a-zA-Z]([._-]?[0-9a-zA-Z]+)*@[0-9a-zA-Z]([._-]?[0-9a-zA-Z]+)*\.[a-zA-Z]{2,}$/i.test(
-      values.email,
-    )
-  ) {
+  if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(values.email)) {
     errors.email = '올바른 이메일 형식이 아닙니다.';
   }
-
-  if (!(values.password.length >= 8 && values.password.length < 20)) {
+  if (!(values.password.length >= 8 && values.password.length <= 20)) {
     errors.password = '비밀번호는 8~20자 사이로 입력해주세요.';
   }
 
   return errors;
 }
 
-export {validateLogin};
+function validateLogin(values: UserInfomation) {
+  return validateUser(values);
+}
+
+function validateSignup(values: UserInfomation & {passwordConfirm: string}) {
+  const errors = validateUser(values);
+  const signupErrors = {...errors, passwordConfirm: ''};
+
+  if (values.password !== values.passwordConfirm) {
+    signupErrors.passwordConfirm = '비밀번호가 일치하지않습니다.';
+  }
+
+  return signupErrors;
+}
+
+export {validateLogin, validateSignup};
